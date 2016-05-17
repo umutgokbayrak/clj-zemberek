82060ba3d223f04a71f8bf3a58aa758c0d24c63e
Umut Gökbayrak <umutgokbayrak@yahoo.com>
2016-05-17 11:22:48 +0300

    updated for clojars commit

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.umutgokbayrak/clj-zemberek "0.4.0"
   :description "Clojure wrapper for Zemberek (NLP for Turkish language)"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/umutgokbayrak/clj-zemberek"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
